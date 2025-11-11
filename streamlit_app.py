c848b068620e46059b252acee01dfe3a1d8286e8
viecent1004 <125888779+viecent1004@users.noreply.github.com>
2025-11-11 16:02:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import clips
+import logging
 
+#setup working environment 
+logging.basicConfig(leverl=15,format='%(message)s')
+
+env=clips.Environment()
+router=clips.LoggingRouter()
+env.add_router(router)
+
+#input
+name=st.text input ("Enter your name")
+
+#knowledge base
+env.build('(deftemplate result(sul)
